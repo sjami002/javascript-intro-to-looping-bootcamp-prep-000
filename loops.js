3c0d01186de4bd11eb9959f658e3006fe144d89f
Shoptorshi Jamil <sjami002@gmail.com>
2019-02-18 03:36:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,3 +14,15 @@ function whileLoop(n) {
     console.log(n--)
   } return "done"
 }
+
+function doWhileLoop(num) {
+  var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+do {
+  console,log("I run once regardless.");
+} while (incrementVariable() < num)
+}
